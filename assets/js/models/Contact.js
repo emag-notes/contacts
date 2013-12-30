91ecdda3a79f213e025e9407a817c25d78ba64f6
emag <lanabe.lanabe@gmail.com>
2013-12-30 14:14:43 +0900

    Add require config

---
+++ assets/js/models/Contact.js
@@ -0,0 +1,6 @@
+define([
+    'backbone'
+], function(Backbone) {
+    var Contact = Backbone.Model.extend({});
+    return Contact;
+});
---
+++ assets/js/require.config.js
@@ -0,0 +1,19 @@
+var require = {
+    baseUrl: '/assets/js',
+    paths: {
+        'jquery': 'vendor/jquery-2.0.3',
+        'jquery.mobile': 'vendor/jquery.mobile-1.4.0',
+        'underscore': 'vendor/underscore',
+        'backbone': 'vendor/backbone',
+        'backbone.localStorage': 'vendor/backbone.localStorage'
+    },
+    shim: {
+        'underscore': {
+            exports: '_'
+        },
+        'backbone': {
+            exports: "Backbone",
+            deps: ['jquery', 'underscore']
+        }
+    }
+};

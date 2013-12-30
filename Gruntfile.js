8c00e397b774eb9ea3959a7ee72ffe06f1d1c4b8
emag <lanabe.lanabe@gmail.com>
2013-12-30 13:44:12 +0900

    initial commit

---
+++ Gruntfile.js
@@ -0,0 +1,62 @@
+module.exports = function(grunt) {
+
+    'use strict';
+
+    [
+        'grunt-contrib-jst',
+        'grunt-contrib-requirejs'
+    ].foreach(function(name) {
+        grunt.loadNpmTasks(name);
+    });
+
+    grunt.initConfig({
+        jst: {
+            options: {
+                processName: function(filename) {
+                    return filename.match(/((?:mobile|pc)\/.*).html$/)[1];
+                },
+                processContent: function(src) {
+                    return src.replace(/(^\s+|\s+$)/gm, '');
+                },
+                amd: true
+            },
+            pc: {
+                files: {
+                    'assets/js/jst/pc.js':
+                    ['assets/js/templates/pc/*.html']
+                }
+            },
+            mobile: {
+                files: {
+                    'assets/js/jst/mobile.js':
+                    ['assets/js/templates/mobile/*.html']
+                }
+            }
+        },
+
+        requirejs: {
+            options: {
+                mainConfigFile: 'assets/js/require.config.js',
+                baseUrl: './assets/js',
+                paths: {
+                    requirejs: 'vendor/require'
+                },
+                include: ['requirejs']
+            },
+            pc: {
+                options: {
+                    out: 'assets/js/dist/pc.min.js',
+                    name: 'pc'
+                }
+            },
+            mobile: {
+                options: {
+                    out: 'assets/js/dist/mobile.min.js',
+                    name: 'mobile'
+                }
+            }
+        }
+
+    });
+
+};
